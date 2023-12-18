fda95f64b0046369762e9d8cea64546cec1ae04c
Jaiabhisshek8 <jaiabhissheks@gmail.com>
2023-12-18 10:04:43 +0530

    Done

---
+++ src/components/Likeimage2.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LikeImage2 = (props) => {
+ const {count, handleCount} = props
+  return (
+    <div>
+      <button onClick={handleCount}>Like Image {count}</button>
+    </div>
+  );
+}
+
+export default LikeImage2;
